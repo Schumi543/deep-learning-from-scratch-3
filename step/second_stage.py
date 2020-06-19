97dae5a561a08b51e33785995b98ba809e7cf572
Schumi543 <12729148+Schumi543@users.noreply.github.com>
2020-06-19 15:30:25 +0900

    impl: no_grad mode

--- step/second_stage.py
+++ step/second_stage.py
@@ -1,6 +1,25 @@
+import contextlib
 import numpy as np
 from typing import List
 import weakref
+
+
+class Config:
+    enable_backprop = True
+
+
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+
+
+def no_grad():
+    return using_config('enable_backprop', False)
 
 
 class Variable:
@@ -68,14 +87,15 @@ class Function:
             ys = (ys,)
         outputs = [Variable(_as_array(y)) for y in ys]
 
-        # update generation
-        self.generation = max([x.generation for x in args])
+        if Config.enable_backprop:
+            # update generation
+            self.generation = max([x.generation for x in args])
 
-        # memorize
-        for output in outputs:
-            output.set_creator(self)
-        self.args = args
-        self.outputs = [weakref.ref(output) for output in outputs]
+            # memorize
+            for output in outputs:
+                output.set_creator(self)
+            self.args = args
+            self.outputs = [weakref.ref(output) for output in outputs]
 
         return outputs if len(outputs) > 1 else outputs[0]
 
