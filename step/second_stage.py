8c3de413e4ade99c9f9b46c2df09fa9af712a9b3
Schumi543 <12729148+Schumi543@users.noreply.github.com>
2020-06-19 22:24:53 +0900

    feat: div

--- step/second_stage.py
+++ step/second_stage.py
@@ -230,6 +230,34 @@ def rsub(x0, x1):
 Variable.__rsub__ = rsub
 
 
+class Div(Function):
+    def forward(self, x0, x1):
+        y = x0 / x1
+        return y
+
+    def backward(self, gy):
+        x0, x1 = self.args[0].data, self.args[1].data
+        gx0 = gy / x1
+        gx1 = gy * (-x0 / x1 ** 2)
+        return gx0, gx1
+
+
+def div(x0, x1):
+    x1 = _as_array(x1)
+    return Div()(x0, x1)
+
+
+Variable.__div__ = div
+
+
+def rdiv(x0, x1):
+    x1 = _as_array(x1)
+    return Div()(x1, x0)
+
+
+Variable.__rdiv__ = rdiv
+
+
 def square(x):
     return Square()(x)
 
