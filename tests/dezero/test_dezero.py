25ad112601384a2ed2c0c15ed311d107dcb68f7b
Schumi543 <12729148+Schumi543@users.noreply.github.com>
2020-06-20 22:44:54 +0900

    refactor: extract math_functions

--- tests/dezero/test_dezero.py
+++ tests/dezero/test_dezero.py
@@ -1,8 +1,6 @@
-import pytest
-from dezero import Variable
-from dezero.math_functions import sphere, matyas, goldstein, sin
-
 import numpy as np
+
+from dezero import Variable
 
 
 def test_add():
@@ -78,39 +76,3 @@ def test_pow():
     assert y.data == 8
 
 
-def test_sphere_function():
-    x = Variable(np.array(1))
-    y = Variable(np.array(1))
-    z = sphere(x, y)
-    z.backward()
-
-    assert (x.grad, y.grad) == (2, 2)
-
-
-def test_matyas_function():
-    x = Variable(np.array(1))
-    y = Variable(np.array(1))
-    z = matyas(x, y)
-    z.backward()
-
-    assert x.grad == pytest.approx(0.04)
-    assert y.grad == pytest.approx(0.04)
-
-
-def test_goldstein_function():
-    x = Variable(np.array(1))
-    y = Variable(np.array(1))
-    z = goldstein(x, y)
-    z.backward()
-
-    assert x.grad == -5376
-    assert y.grad == 8064
-
-
-def test_sin():
-    x = Variable(np.array(np.pi / 4))
-    y = sin(x)
-    y.backward()
-
-    assert y.data == pytest.approx(0.707106)
-    assert x.grad == pytest.approx(0.707103)
