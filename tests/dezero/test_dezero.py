af2f08bbde1ec483dd0f441df3735c58b7f876e5
Schumi543 <12729148+Schumi543@users.noreply.github.com>
2020-06-20 21:13:14 +0900

    test: special math functions

--- tests/dezero/test_dezero.py
+++ tests/dezero/test_dezero.py
@@ -75,3 +75,51 @@ def test_pow():
     y = x ** 3
 
     assert y.data == 8
+
+
+def test_sphere_function():
+    # noinspection PyShadowingNames
+    def sphere(x, y):
+        z = x ** 2 + y ** 2
+        return z
+
+    x = Variable(np.array(1))
+    y = Variable(np.array(1))
+    # noinspection PyTypeChecker
+    z = sphere(x, y)
+    z.backward()
+
+    assert (x.grad, y.grad) == (2, 2)
+
+
+def test_matyas_function():
+    # noinspection PyShadowingNames
+    def matyas(x, y):
+        z = 0.26 * (x ** 2 + y ** 2) - 0.48 * x * y
+        return z
+
+    x = Variable(np.array(1))
+    y = Variable(np.array(1))
+    # noinspection PyTypeChecker
+    z = matyas(x, y)
+    z.backward()
+
+    assert x.grad == pytest.approx(0.04)
+    assert y.grad == pytest.approx(0.04)
+
+
+def test_goldstein_function():
+    # noinspection PyShadowingNames
+    def goldstein(x, y):
+        z = (1 + (x + y + 1) ** 2 * (19 - 14 * x + 3 * x ** 2 - 14 * y + 6 * x * y + 3 * y ** 2)) * \
+            (30 + (2 * x - 3 * y) ** 2 * (18 - 32 * x + 12 * x ** 2 + 48 * y - 36 * x * y + 27 * y ** 2))
+        return z
+
+    x = Variable(np.array(1))
+    y = Variable(np.array(1))
+    # noinspection PyTypeChecker
+    z = goldstein(x, y)
+    z.backward()
+
+    assert x.grad == -5376
+    assert y.grad == 8064
