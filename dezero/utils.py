6257603912c75506b5f54a9c579612741806cffe
Schumi543 <12729148+Schumi543@users.noreply.github.com>
2020-06-20 22:17:32 +0900

    feat: plot_dot_graph

--- dezero/utils.py
+++ dezero/utils.py
@@ -1,3 +1,6 @@
+import os
+import subprocess
+
 from dezero.core_simple import Variable, Function
 
 
@@ -19,3 +22,46 @@ def _dot_func(f: Function):
         txt += f'{id(f)} -> {id(y())}\n'
 
     return txt
+
+
+def get_dot_graph(output: Variable, verbose=True):
+    txt = ''
+    funcs = []
+    seen_set = set()
+
+    def add_func(f: Function):
+        if f not in seen_set:
+            funcs.append(f)
+            seen_set.add(f)
+
+    add_func(output.creator)
+    txt += _dot_var(output, verbose)
+
+    while funcs:
+        func = funcs.pop()
+        txt += _dot_func(func)
+        for x in func.args:
+            txt += _dot_var(x, verbose)
+
+            if x.creator is not None:
+                add_func(x.creator)
+
+    return 'digraph g {\n' \
+           f'{txt}' \
+           '}'
+
+
+def plot_dot_graph(output: Variable, verbose=True, to_file='graph.png'):
+    dot_graph = get_dot_graph(output, verbose)
+
+    tmp_dir = os.path.join(os.path.expanduser('~'), '.dezero')
+    if not os.path.exists(tmp_dir):
+        os.mkdir(tmp_dir)
+    graph_path = os.path.join(tmp_dir, 'tmp_graph.dot')
+
+    with open(graph_path, 'w') as f:
+        f.write(dot_graph)
+
+    extension = os.path.splitext(to_file)[1][1:]
+    cmd = f'dot {graph_path} -T {extension} -o {to_file}'
+    subprocess.run(cmd, shell=True)
